b0ca151147b2f9dab11f6d75f4e321d59e5f4514
龍之介 前川 <190443@jc-21.jp>
2021-11-19 11:12:06 +0900

    Button.Text変更

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -18,6 +18,58 @@ namespace R03BMI
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            String height = heighit.Text;
+            String weight = weighit.Text;
+            try
+            {
+                double h = double.Parse(height);
+                double w = double.Parse(weight);
+
+                if (h > 10)
+                {
+                    h = h / 100;
+                }
+
+                if(w > 5000)
+                {
+                    w = w / 1000;
+                }
+
+
+                double bmi = w / h / h;
+
+                bmi = (Math.Round(bmi,1, MidpointRounding.AwayFromZero));
+
+               
+
+                if (bmi < 18.5)
+                {
+                    result.TextText = bmi + "低体重（瘦せ）";
+                }
+                else if (bmi < 25)
+                {
+                    result.Text = bmi + "普通体重";
+                }
+                else if (bmi < 30)
+                {
+                    result.Text = bmi + "肥満（1度）";
+                }
+                else if (bmi < 35)
+                {
+                    result.Text = bmi + "肥満（2度）";
+                }
+                else if (bmi < 40)
+                {
+                    result.Text = bmi + "肥満（3度）";
+                }
+                else if (bmi >= 40)
+                {
+                    result.Text = bmi + "肥満（4度）";
+                }
+            }catch (FormatException ex)
+            {
+                Console.WriteLine(ex);
+            }
 
         }
     }
